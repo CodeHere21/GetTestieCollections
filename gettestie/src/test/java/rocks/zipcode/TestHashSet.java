d747a789d69d442a631113ee8401bf26dce40f0e
lena <alena.litouka@gmail.com>
2021-03-21 14:36:30 -0400

    12 tests passed

--- gettestie/src/test/java/rocks/zipcode/TestHashSet.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashSet.java
@@ -3,6 +3,27 @@ import org.junit.Assert;
 import org.junit.Test;
 import junit.framework.TestCase;
 
+import java.util.HashSet;
+
 public class TestHashSet{
+    @Test
+    public void TestHashSet1() {
+        HashSet<String> h = new HashSet<String>();
+        h.add("Hello World");
+        Assert.assertEquals(false, h.isEmpty());
+    }
+    @Test
+    public void TestHashSet2(){
+        HashSet<String> h=new HashSet<String>();
+        h.add("Hello World");
+        h.remove("Hello World");
+        Assert.assertEquals(true,h.isEmpty());
+    }
+    @Test
+    public void TestHashSet3() {
+        HashSet<String> h = new HashSet<String>();
+        h.add("Hello World");
+        Assert.assertEquals(true, h.contains("Hello World"));
+    }
 
 }
